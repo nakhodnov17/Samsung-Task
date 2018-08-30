8fa3886b9cdeda292a1471aca7e92aa42f67f680
nakhodnov17 <nakhodnov17@gmai.com>
2018-08-30 21:54:29 +0300

    Add exps 32-36

---
+++ Stein Gradients/Experiments/runner.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python ./run_experiment.py --config_name=config_model_33
+python ./run_experiment.py --config_name=config_model_34
+python ./run_experiment.py --config_name=config_model_35
+python ./run_experiment.py --config_name=config_model_36
+python ./run_experiment.py --config_name=config_model_32
